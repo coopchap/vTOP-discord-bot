96b4a6ad70f1d9c19381637a9f51bda121e9aaf5
Cooper Chapple <coopchap@icloud.com>
2023-10-13 13:18:12 -0400

    Started /feature functionality

--- DiscordBot/commands/feature.js
+++ DiscordBot/commands/feature.js
@@ -9,7 +9,17 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
+import { read, writeFileXLSX } from "xlsx";
+
+/* load the codepage support library for extended support with older formats  */
+import { set_cptable } from "xlsx";
+import * as cptable from 'xlsx/dist/cpexcel.full.mjs';
+set_cptable(cptable);
+
+
 export function addTrackedFeature(interaction) {
-    console.log("Feature is now tracked")
-    interaction.reply('Feature now tracked');
+    var data = XLSX.read(workbook, opts);
+    var cell = {c:0, r:0}
+    XLSX.utils.sheet_add_aoa(worksheet, ["hello"], { origin: cell });
+    console.log(cell.v);
 }
