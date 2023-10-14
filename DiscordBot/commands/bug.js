31ad7f2d624dbd61afe6206c3a63daf27a5a2efc
Cooper Chapple <coopchap@icloud.com>
2023-10-14 00:53:56 -0400

    deleted codepage support libraru

--- DiscordBot/commands/bug.js
+++ DiscordBot/commands/bug.js
@@ -9,12 +9,6 @@ const client = new Client({
 
 import { read, writeFileXLSX } from "xlsx";
 
-/* load the codepage support library for extended support with older formats  */
-import { set_cptable } from "xlsx";
-import * as cptable from 'xlsx/dist/cpexcel.full.mjs';
-set_cptable(cptable);
-
-
 export function addTrackedBug(interaction) {
     const bugID = getNextBugID(interaction);
     addBugToExcel(interaction, bugID);
