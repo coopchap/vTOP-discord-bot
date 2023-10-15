d120c58e0ffa3a1143b1faba8f2ee30e60e89c97
coopchap <140867101+coopchap@users.noreply.github.com>
2023-10-15 04:19:24 +0000

    update .gitignore. Removed all but /bug for test

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ import { finishedRequestReport } from './src/commands/finished.js'; */
 client.on(Events.ClientReady, (x) => {
     console.log(`${x.user.tag} is ready!`);
 
-    const feature = new SlashCommandBuilder()
-        .setName('feature')
-        .setDescription('This creates a trackable feature request and renames the thread\'s title')
-        .addStringOption(option =>
-            option
-                .setName('description')
-                .setDescription('The description of the feature request'));
-
     const bug = new SlashCommandBuilder()
         .setName('bug')
         .setDescription('This creates a trackable bug report and renames the thread\'s title')
@@ -47,44 +39,9 @@ client.on(Events.ClientReady, (x) => {
                 .setName('reporter')
                 .setDescription('Who submitted the report')); */
 
-    const improvement = new SlashCommandBuilder()
-        .setName('improvement')
-        .setDescription('This creates a trackable improvement request and renames the thread\'s title')
-        .addStringOption(option =>
-            option
-                .setName('description')
-                .setDescription('The description of the bug report'))
-        .addStringOption(option =>
-            option
-            .setName('attachements')
-            .setDescription('Links to relevant attachements'));
-
-    const declined = new SlashCommandBuilder()
-        .setName('declined')
-        .setDescription('This declines the request/report in the current thread')
-        .addStringOption(option =>
-            option
-            .setName('reason')
-            .setDescription('The reason for declination'));
-
-    const approved = new SlashCommandBuilder()
-        .setName('approved')
-        .setDescription('This approves the request/report in the current thread, meaning development on it will start')
-
-    const finished = new SlashCommandBuilder()
-        .setName('finished')
-        .setDescription('This closes the thread and notifies the requester/reporter that the development is live.')
-        .addStringOption(option =>
-            option
-                .setName('additional info')
-                .setDescription('Additional information')); 
-
-    client.application.commands.create(feature);
+   
     client.application.commands.create(bug);
-    client.application.commands.create(improvement);
-    client.application.commands.create(declined);
-    client.application.commands.create(approved);
-    client.application.commands.create(finished);
+
 });
 
 client.on('interactionCreate', (interaction) => {
