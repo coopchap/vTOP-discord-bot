aaf6366302a3963f46b2571552d07bfc1edeae00
coopchap <140867101+coopchap@users.noreply.github.com>
2023-10-16 03:28:41 +0000

    thread renamed, replies w/ messgaes

--- src/commands/bug.js
+++ src/commands/bug.js
@@ -1,7 +1,6 @@
 import {
     Client,
-    GatewayIntentBits,
-    userMention
+    GatewayIntentBits
 } from 'discord.js';
 
 const client = new Client({
@@ -12,15 +11,16 @@ export function addTrackedBug(interaction, client) {
     const bugID = getNextBugID(interaction); //gets current bug id, ex: '1'
     const viewBugID = formatBugID(bugID, interaction)[0]; //ex: 'vCGP-001'
     const excelBugID = formatBugID(bugID, interaction)[1]; //ex: '001'
+
     reply(interaction, viewBugID, client); //provides message in thread
-    // renameThread(interaction, viewBugID); //renames thread with id
+    renameThread(interaction, viewBugID); //renames thread with id
     addBugToExcel(interaction, bugID, excelBugID); //adds bug to .xlsx
-    console.log(excelBugID);
+    
 }
 
 function getNextBugID(interaction) {
 
-    /* const fs = require('fs');  -- Commented out since this isn't valid to be run in browser
+    /* const fs = require('fs'); 
 
     let currentBugID;
     fs.promises.readFile('../../tracking/lastID.json', 'utf8')
@@ -40,7 +40,7 @@ function getNextBugID(interaction) {
 
     return currentBugID;
     */
-    return 64; //temporary
+    return 65; //temporary, cant be run in browser
 }
 
 function formatBugID(bugID, interaction) {
@@ -100,17 +100,27 @@ function addBugToExcel(interaction, bugID, viewBugID) {
 }
 
 function renameThread(interaction, viewBugID) {
-    console.log('renamed');
-    const channelID = interaction.channelId;  //gets thread name vvvv
-    const thread = client.channels.cache.get(channelID);
-    let threadName = thread.name;
-    thread.name = '[' + viewBugID + '] ' + threadName; //updates name
+    const forumPost = interaction.channel;
+    const forumPostName = forumPost.name;
+    const newThreadName = "[" + viewBugID + "] " + forumPostName; 
+
+    forumPost.setName(newThreadName);
+    
+    forumPost.edit({name: newThreadName});
 }
 
 async function reply(interaction, viewBugID, client) { //adds message about feature now being tracked
-    const reporter = interaction.options.getUser('reporter'); //get user selected when cmommand sent
-    await interaction.reply('<@' + reporter + '>, your bug report is now being tracked! A ticket is has been filed. You will be notified in this thread if the issue is resolved.');
+    const bugTitle = renameThread(interaction, viewBugID);
+    const bugDescription = interaction.options.getString('description');
+    const attatchmentsNumber = 0;
+    const bugCreationDate = interaction.options.getString('date');
+
+    await interaction.reply({content: `You have create a new bug ticket.\nID: \`${viewBugID}\`.\nTitle: \`${bugTitle}\`.\nDescription: \`${bugDescription}\`.\nAttatchments: \`${attatchmentsNumber}\`.\nCreated: ${bugCreationDate}`, ephemeral: true});
+
     const channelID = interaction.channelId;  //gets additional message vvvv
     const channel = client.channels.cache.get(channelID);
-    channel.send('Your ticked number is: **' + viewBugID + '**');
+
+    const reporter = interaction.options.getUser('reporter'); //get user selected when cmommand sent
+    channel.send('<@' + reporter + '>, your bug report is now being tracked! A ticket is has been filed. You will be notified in this thread if the issue is resolved.');
+    channel.send('Your ticket number is: **' + viewBugID + '**');
 }
